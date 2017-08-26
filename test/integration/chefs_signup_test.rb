99dabe9908a7044c286500b2adba4a5392104fe3
Cami Hayes <camihayes72@gmail.com>
2017-08-26 22:55:55 +0000

    create chefs show action with tests

--- test/integration/chefs_signup_test.rb
+++ test/integration/chefs_signup_test.rb
@@ -8,7 +8,7 @@ class ChefsSignupTest < ActionDispatch::IntegrationTest
   end
   
   test "reject an invalid signup" do
-    get sign_up path
+    get signup_path
     assert_no_difference "Chef.count" do
       post chefs_path, params: {chef: {chefname: " ", email: " ", password: "password",
                                       password_confirmation: " " }}
@@ -19,7 +19,7 @@ class ChefsSignupTest < ActionDispatch::IntegrationTest
   end
   
   test "accept valid signup" do
-     get sign_up path
+     get signup_path
     assert_difference "Chef.count", 1 do
       post chefs_path, params: {chef: {chefname: "cami", email: "camihayes72@gmail.com", password: "password",
                                       password_confirmation: "password" }}
