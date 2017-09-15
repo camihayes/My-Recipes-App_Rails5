0e40e5e8ef7efb8e4026691a743ff1821b3f8c4a
Cami Hayes <camihayes72@gmail.com>
2017-09-15 23:24:54 +0000

    complete authenticate system with tests

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -4,9 +4,12 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   def setup
     @chef = Chef.create!(chefname: "cami", email: "cami@example.com",
                            password: "password", password_confirmation: "password")
+    @chef2 = Chef.create!(chefname: "john", email: "john@example.com",
+                           password: "password", password_confirmation: "password")
   end
   
    test "reject an invalid edit" do
+    sign_in_as(@chef, "password") 
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
     patch chef_path(@chef), params: {chef: {chefname: " ", email: "cami@example.com"}}
@@ -15,7 +18,8 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.panel-body'
   end
   
-  test "accept valid edit" do
+  test "accept valid signup" do
+    sign_in_as(@chef, "password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
     patch chef_path(@chef), params: {chef: {chefname: "cami1", email: "cami1@example.com"}}
